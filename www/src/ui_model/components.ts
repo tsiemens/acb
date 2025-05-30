200b58b4b1e3cb755d3bcab7844dae66bf122405
Trevor Siemens <tsiemens@users.noreply.github.com>
2025-05-30 08:19:40 -0700

    [Web UI] Various minor improvements
    - Fixed/improved row coloring to distinguish actions
    - Added year highlighting in output
    - Ugraded collapsible regions to be a bit more compact/nicer

--- www/src/ui_model/components.ts
+++ www/src/ui_model/components.ts
@@ -1,8 +1,7 @@
 import { ElementModel } from "./model_lib.js";
 
 export class CollapsibleRegion extends ElementModel {
-    public static readonly BUTTON_CLASS: string = "collapsible-region-btn";
-    public static readonly CONTENT_CLASS: string = "collapsible-region-content";
+    public static readonly BUTTON_CLASS: string = "collapsible-content-btn";
 
     public static initAll() {
         const collapsibles = document.querySelectorAll(`.${CollapsibleRegion.BUTTON_CLASS}`);
@@ -12,18 +11,34 @@ export class CollapsibleRegion extends ElementModel {
                 if (!event.target) {
                     return;
                 }
-                let button = (event.target as Element)
-                    .closest(`.${CollapsibleRegion.BUTTON_CLASS}`) as HTMLElement;
-                button.classList.toggle('active');
-                const content = button.nextElementSibling as HTMLElement | null;
-                if (content) {
-                    if (content.style.maxHeight) {
-                        content.style.maxHeight = '';
-                    } else {
-                        content.style.maxHeight = content.scrollHeight.toString() + 'px';
-                    }
-                }
+                CollapsibleRegion.doToggle(event);
             });
         });
+    }
+
+    public static doToggle(buttonEvent: Event) {
+        if (!buttonEvent.target) {
+            return;
+        }
+        const button = (buttonEvent.target as Element)
+            .closest(`.${CollapsibleRegion.BUTTON_CLASS}`) as HTMLElement;
+        const content = button.nextElementSibling as HTMLElement | null;
+
+        button.classList.toggle('active');
+
+        const COLLAPSED = 'collapsed';
+        const EXPANDED = 'expanded';
+
+        if (content) {
+            if (content.classList.contains(COLLAPSED)) {
+                content.classList.remove(COLLAPSED);
+                content.classList.add(EXPANDED);
+                button.classList.add(EXPANDED);
+            } else {
+                content.classList.remove(EXPANDED);
+                content.classList.add(COLLAPSED);
+                button.classList.remove(EXPANDED);
+            }
+        }
     }
 }
