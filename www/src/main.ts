eb1fe029f135bc3e697b05c345ffceffcd1642ec
Trevor Siemens <tsiemens@users.noreply.github.com>
2026-03-10 08:02:00 -0700

    Add UI components and base data store for new vue FileManagerDrawer
    This isn't doing anything yet, just there, looking pretty.
    
    This should end up replacing the inline uploaded files view, currently
    under the drag/drop area. It will be able to handle files of different
    kinds, and from the various different 'apps' that we should end up
    adding later, such as xlsx files, pdfs, etc.
    
    Has plenty of nice UX features already, so it sort of behaves like a
    normal file explorer.

--- www/src/main.ts
+++ www/src/main.ts
@@ -6,6 +6,8 @@ import { WasmVersionDisplay } from './ui_model/misc.js';
 import { ErrorBox } from './ui_model/error_displays.js';
 import { getErrorBoxStore } from './vue/error_box_store.js';
 import ErrorBoxVue from './vue/ErrorBox.vue';
+import { getFileManagerStore, FileKind } from './vue/file_manager_store.js';
+import FileManagerDrawer from './vue/FileManagerDrawer.vue';
 
 function createVueApps(): void {
    // Inject components which have been converted to Vue apps.
@@ -19,6 +21,19 @@ function createVueApps(): void {
       store: getErrorBoxStore(ErrorBox.GIT_ERRORS_ID),
       width: '100%',
    }).mount(`#${ErrorBox.GIT_ERRORS_ID}`);
+
+   const fileManagerStore = getFileManagerStore();
+   // Sample files. TODO delete this.
+   const d = (k: FileKind) => FileKind.isDownloadableDefault(k);
+   fileManagerStore.addFile( { name: 'transactions_2024.csv',           kind: FileKind.AcbTxCsv,      isDownloadable: d(FileKind.AcbTxCsv),      useChecked: true,  data: new Uint8Array() });
+   fileManagerStore.addFile( { name: 'transactions_2023.csv',           kind: FileKind.AcbTxCsv,      isDownloadable: d(FileKind.AcbTxCsv),      useChecked: false, data: new Uint8Array() });
+   fileManagerStore.addFile( { name: 'acb_export_2024-01-15T12-00.zip', kind: FileKind.AcbOutputZip,  isDownloadable: d(FileKind.AcbOutputZip),  useChecked: false, data: new Uint8Array() });
+   fileManagerStore.addFile( { name: 'acb_export_2023-12-31T08-30.zip', kind: FileKind.AcbOutputZip,  isDownloadable: d(FileKind.AcbOutputZip),  useChecked: false, data: new Uint8Array() });
+   fileManagerStore.addFile( { name: 'summary_output.txt',              kind: FileKind.OutputText,    isDownloadable: d(FileKind.OutputText),    useChecked: false, data: new Uint8Array() });
+   fileManagerStore.addFile( { name: 'questrade_export.xlsx',           kind: FileKind.QuestradeXlsx, isDownloadable: d(FileKind.QuestradeXlsx), useChecked: true,  data: new Uint8Array() });
+   fileManagerStore.addFile( { name: 'unrecognized_file.dat',           kind: FileKind.Other,         isDownloadable: d(FileKind.Other),         useChecked: false, data: new Uint8Array(), warning: 'File type could not be determined' });
+   fileManagerStore.hasNotification = true;
+   createApp(FileManagerDrawer, { store: fileManagerStore }).mount('#fileManagerApp');
 }
 
 /**
