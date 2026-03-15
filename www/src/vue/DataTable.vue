4d182f98f98aafbe4c068c82ada7e9bc9266b03c
Trevor Siemens <tsiemens@users.noreply.github.com>
2026-03-15 10:07:36 -0700

    Refactor DataTable to own styles properly, and be reused

--- www/src/vue/DataTable.vue
+++ www/src/vue/DataTable.vue
@@ -4,6 +4,7 @@
 
     <div v-if="errors.length > 0" class="security-errors">
       <p v-for="(err, i) in errors" :key="i">{{ err }}</p>
+      <p v-if="errorSuffix">{{ errorSuffix }}</p>
     </div>
 
     <div class="table-fixed-head">
@@ -14,8 +15,15 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(row, ri) in table.rows" :key="ri">
+          <tr
+            v-for="(row, ri) in table.rows"
+            :key="ri"
+            :class="rowClassFn ? rowClassFn(row) : ''"
+          >
             <td v-for="(cell, ci) in row" :key="ci">{{ cell }}</td>
+          </tr>
+          <tr v-if="table.footer">
+            <td v-for="(cell, ci) in table.footer" :key="ci">{{ cell }}</td>
           </tr>
         </tbody>
       </table>
@@ -42,6 +50,14 @@ export default defineComponent({
          type: String,
          default: '',
       },
+      errorSuffix: {
+         type: String,
+         default: '',
+      },
+      rowClassFn: {
+         type: Function as PropType<(row: string[]) => string | string[]>,
+         default: null,
+      },
    },
    setup(props) {
       const errors = computed(() => props.table.errors || []);
@@ -50,3 +66,68 @@ export default defineComponent({
    },
 });
 </script>
+
+<style scoped>
+.table-title {
+  padding-left: 8px;
+  background: var(--dark-color);
+  color: white;
+  border-radius: 5px;
+  margin-bottom: 5px;
+  margin-top: 20px;
+  font-weight: bold;
+}
+
+.security-errors {
+  border: 1px solid #f5c6cb;
+  background: #fbe6e7;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.security-notes {
+  border: 1px solid #c7c7c7;
+  background: #f9f9f9;
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 5px;
+}
+
+.table-fixed-head {
+  overflow-y: auto;
+  max-height: 500px;
+}
+.table-fixed-head thead th {
+  position: sticky;
+  top: 0;
+}
+.table-fixed-head table {
+  border-collapse: collapse;
+  width: 100%;
+}
+.table-fixed-head th,
+.table-fixed-head td {
+  padding: 8px 16px;
+  font-size: 9pt;
+}
+.table-fixed-head th {
+  background: #ededed;
+  color: black;
+  z-index: 1;
+}
+.table-fixed-head td {
+  box-shadow: inset 1px 1px #999;
+}
+.table-fixed-head th {
+  box-shadow: inset 1px 1px #999;
+}
+.table-fixed-head thead {
+  box-shadow: inset 0px -1px #999;
+}
+.table-fixed-head table {
+  border-right: solid 1px #999;
+  border-bottom: solid 1px #999;
+}
+
+</style>
