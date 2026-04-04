dc4022d6cae998fff846d7bf3c93f78580e7ff35
Trevor Siemens <tsiemens@users.noreply.github.com>
2026-04-03 17:06:08 -0700

    Fix github build issues
    - wasm when target dir was stale locally, leaving bad files around
    - no node installed prior to running cli pdf tests

--- src/util.rs
+++ src/util.rs
@@ -3,12 +3,14 @@ pub mod date;
 pub mod decimal;
 pub mod http;
 pub mod math;
-pub mod node;
-pub mod py;
 pub mod rc;
 pub mod rw;
 pub mod sys;
 pub mod zip;
 
 #[cfg(not(target_arch = "wasm32"))]
+pub mod node;
+#[cfg(not(target_arch = "wasm32"))]
 pub mod os;
+#[cfg(not(target_arch = "wasm32"))]
+pub mod py;
