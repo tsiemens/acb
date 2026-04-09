b9d28cea3e161ca242449303c289fbd4fddb7e39
Trevor Siemens <tsiemens@users.noreply.github.com>
2026-04-08 18:09:44 -0700

    www: Refactor/improve table filter options

--- www/src/vue/OutputArea.vue
+++ www/src/vue/OutputArea.vue
@@ -4,7 +4,7 @@
       <h2 class="output-title">Results</h2>
     </div>
 
-    <CollapsibleAcbAppOutputViewSettings :store="store" />
+    <AcbAppOutputViewSettings :store="store" />
 
     <div class="view-mode-toggle">
       <button
@@ -62,11 +62,11 @@ import { defineComponent, type PropType } from 'vue';
 import { type OutputStore, AcbOutputViewMode, getViewModeLabel } from './output_store.js';
 import DataTable from './DataTable.vue';
 import SecurityTablesOutput from './SecurityTablesOutput.vue';
-import CollapsibleAcbAppOutputViewSettings from './CollapsibleAcbAppOutputViewSettings.vue';
+import AcbAppOutputViewSettings from './AcbAppOutputViewSettings.vue';
 
 export default defineComponent({
    name: 'OutputArea',
-   components: { DataTable, SecurityTablesOutput, CollapsibleAcbAppOutputViewSettings },
+   components: { DataTable, SecurityTablesOutput, AcbAppOutputViewSettings },
    props: {
       store: {
          type: Object as PropType<OutputStore>,
